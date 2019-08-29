d1618b1ac882f282a8b97c24bd37e330fc958966
wking1987 <fjklfjkl@163.com>
2019-08-29 09:57:28 +0800

    add branch_test.php

---
+++ branch_test.php
@@ -0,0 +1,10 @@
+﻿<?php
+return array(
+	'username' => 'tom',
+	'password' => '123123',
+	'alikey' => '3333',
+	'wechat' => 'this is master branch',
+	'ali' => 'this is wechat branch',
+	'local' => 'git hub',
+);
+?>
